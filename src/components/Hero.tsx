64aabe908eba95b91ec88b65b6c6f6a08651e6bd
shazib <shazib@Shazib.shazib>
2024-04-07 18:45:10 +0600

    responsive hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,33 +1,31 @@
 import 'aos/dist/aos.css';
-import mobile from '../assets/Screenshot_1.png';
 
 const Hero = () => {
   return (
-    <section
-      className="section-padding  gap-4 place-items-center relative py-24 md:py-0 p-10 pt-24 lg:mt-[17vh] body-bg h-[100%] "
-      id="hero"
-    >
-      <img
-        src={mobile}
-        alt=""
-        className="hidden lg:block absolute -right-24 top-10"
-      />
-      <div className="lg:grid place-items-center space-y-10 relative">
-        <h1 className="text-2xl lg:text-7xl font-bold  mb-24 text-center top-10">
-          Industry-leading stolen <br /> vehicle tracking solutions
-        </h1>
+    <>
+      <section
+        className="section-padding place-items-center relative py-24 md:py-0 p-10 pt-24 body-bg md:h-[100vh] "
+        id="hero"
+      >
+        <div className="lg:grid place-items-center space-y-10 relative">
+          <h1 className="text-2xl lg:text-7xl font-bold md:mt-50 mb-24 text-center ">
+            Industry-leading stolen <br /> vehicle tracking solutions
+          </h1>
+        </div>
+      </section>
+      <section className="grid place-items-center md:mb-30 md:mt-20">
         <iframe
           src="https://www.youtube.com/embed/XooGJcxzA_c?si=hAWQm-ZFZ_SFWoq1"
-          className="rounded-[0.9rem] mx-auto w-[70%] h-[33rem] max-sm:w-[100%] max-sm:h-[16rem] drop-shadow-xl shadow-gray-500 margin-this-video"
+          className="rounded-[0.9rem] mx-auto w-[60%] h-[33rem] max-sm:w-[90%] max-sm:h-[16rem] drop-shadow-xl shadow-gray-500 "
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
         ></iframe>
-        <button className="bg-red-500 text-white px-10 py-3 text-2xl rounded">
+        <button className="bg-red-500 text-white px-10 py-3 text-2xl rounded mt-15">
           BECOME A DEALER
         </button>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
